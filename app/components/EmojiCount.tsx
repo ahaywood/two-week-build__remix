7ce18b2b0d74b30c386d53a6e26baa7d607ab31c
Amy Haywood Dutton <me@amyhaywood.com>
2024-02-26 20:32:31 -0600

    💄 Made emoji count full width

--- app/components/EmojiCount.tsx
+++ app/components/EmojiCount.tsx
@@ -22,7 +22,7 @@ const EmojiCount = ({
       <input type="hidden" name="user_id" value={current_user_id} />
       <input type="hidden" name="update_id" value={update_id} />
       <button
-        className={`text-lg  font-mono font-bold h-8 center border-2 rounded-3xl px-3 py-2 gap-2 hover:bg-codGray ${
+        className={`text-lg w-full font-mono font-bold h-8 center border-2 rounded-3xl px-3 py-2 gap-2 hover:bg-codGray ${
           defaultSelected
             ? "border-springBud text-springBud"
             : "border-codGray text-white"
