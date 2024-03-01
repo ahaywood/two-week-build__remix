5fbc602a544a5c0e299656081557ccca284910bf
Amy Haywood Dutton <me@amyhaywood.com>
2024-03-01 02:58:04 -0600

    🚧 WIP: Stubbed out new project page

--- app/routes/_app.projects.new/route.tsx
+++ app/routes/_app.projects.new/route.tsx
@@ -1,3 +1,31 @@
+import { Form } from "@remix-run/react";
+import Banner from "~/components/Banner";
+import { Icon } from "~/components/Icon";
+
 export default function Index() {
-  return <div>New Project</div>;
+  return (
+    <div>
+      <Banner>
+        <h1>New Project</h1>
+      </Banner>
+
+      <Form method="post">
+        <div className="field">
+          <label htmlFor="name">Project Title</label>
+          <input type="text" name="name" placeholder="" />
+        </div>
+
+        <div className="field">
+          <label htmlFor="description">Project Description</label>
+          <textarea name="description"></textarea>
+        </div>
+
+        <div className="field">
+          <button type="submit">
+            <Icon name="arrow">Submit</Icon>
+          </button>
+        </div>
+      </Form>
+    </div>
+  );
 }
