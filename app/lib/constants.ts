253226aa92dfdba4017795e1b5737fb1725b9829
Amy Haywood Dutton <me@amyhaywood.com>
2024-03-01 18:03:12 -0600

    🐛 Added www to constants file

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -3,9 +3,9 @@
 const constants = Object.freeze({
   OG_TITLE: "Two Week Build",
   OG_DESCRIPTION: "A Two Week Build Challenge. The rules are simple. Pick something to build. Build it in 2 weeks. Ship it.",
-  OG_DOMAIN: "https://twoweekbuild.com",
-  OG_IMAGE: "https://twoweekbuild.com/images/og-image.png",
-  BASE_URL: "https://twoweekbuild.com",
+  OG_DOMAIN: "https://www.twoweekbuild.com",
+  OG_IMAGE: "https://www.twoweekbuild.com/images/og-image.png",
+  BASE_URL: "https://www.twoweekbuild.com",
 });
 
 export { constants }
