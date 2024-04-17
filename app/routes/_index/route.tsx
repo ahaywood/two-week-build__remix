ec61daf83d430643b77081882baf0672453af6ef
Amy Haywood Dutton <me@amyhaywood.com>
2024-04-17 17:11:38 -0500

    🐛 Prevented double login buttons

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -65,7 +65,6 @@ export default function Index() {
         <div className="absolute right-6 top-6 flex items-center gap-3">
           <Search />
           <SignUpButton />
-          <LoginButton isUserLoggedIn={false} />
           <LoginButton isUserLoggedIn={!!data.user} />
         </div>
       </header>
